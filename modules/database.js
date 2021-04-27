8dc7d08db241eadbf022ea6e3ed66f4b863651fd
antioch28 <45645363+antioch28@users.noreply.github.com>
2021-04-27 09:30:59 -0600

    String de conexión a Mongo Atlas

--- modules/database.js
+++ modules/database.js
@@ -3,15 +3,13 @@ var mongoose = require('mongoose');
 var servidor = 'localhost:27017';
 var db = 'code-storage';
 
-// var dbUrl = `mongodb+srv://code-storage-admin:uBVbGTQx0TNqx5N6@cluster0.yd1vv.mongodb.net/?retryWrites=true&w=majority`;
-// var dbUrl = process.env.DBURL || `mongodb://${servidor}/${db}`;
-var dbUrl = `mongodb://${servidor}/${db}`;
+var dbUrl = process.env.DB_URL || `mongodb://${servidor}/${db}`;
 
 class Database {
     constructor() {
         mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => {
-                console.log('Se conecto a mongodb atlas');
+                console.log('Connected to MongoDB');
             }).catch((error) => {
                 console.log(error);
             });
