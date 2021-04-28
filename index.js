67f77c184e92147f5938fd2c44de69afcc477872
antioch28 <45645363+antioch28@users.noreply.github.com>
2021-04-28 07:25:04 -0600

    Ajustes

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ var plansRouter = require('./routers/plans-router');
 var app = express();
 
 app.use(cors());
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
